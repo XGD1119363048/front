c59ddf6933cb7209c15b715ddc696dd0d77fc04f
Teresaxyz <294569840@qq.com>
2019-06-17 19:23:25 +0800

    first commit

---
+++ src/utils/api-config.js
@@ -0,0 +1,50 @@
+// const server = 'http://localhost:8081/';
+ //const server = 'http://web.ngrok.cutrain.top:8081/';
+//const server = "https://easy-mock.com/mock/5c7fb3586498b753ed1f9cd0/vap/";
+//const site = window.location.origin;
+//const server = site.substr(0, site.length-5) + ':8081/';
+const server = 'http://10.141.209.225:8081/';
+
+
+
+const api = {
+  server:server,
+  /**
+   * project
+   */
+  projectView : server + "project/view",
+  projectCreate : server + "project/create",
+  projectRename : server + "project/change",
+  projectDelete : server + "project/delete",
+
+  /**
+   * graph
+   */
+  graphGet : server + "graph/get",
+  graphSave : server + "graph/save",
+  graphRun : server + "graph/run",
+  graphProgress : server + "graph/progress",
+  graphStop : server + "graph/stop",
+  graphSample : server + "graph/sample",
+  graphInit : server + "graph/init",
+
+  /**
+   * data
+   */
+  dataUpload : server + "data/upload",
+  dataGet : server + "data/get",
+  dataMove : server + "data/move",
+  dataDelete : server + "data/delete",
+  dataView : server + "data/view",
+  dataCreateSet : server + "data/createset",
+  dataDownload: server + "data/download",
+
+  /**
+   * component
+   */
+  componentListView : server + "component/list",
+  componentParameter : server + "component/param",
+}
+
+export default api;
+
